fb8793b2d71304ee92ffd86546d8e10237641637
2pk1_Jacenko <143966681+adlik2033@users.noreply.github.com>
2026-06-16 16:14:32 +0300

    1

--- API0093BK/Program.cs
+++ API0093BK/Program.cs
@@ -129,7 +129,7 @@ var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 app.UseCors("AllowAll");
 app.UseAuthentication();
 app.UseAuthorization();
